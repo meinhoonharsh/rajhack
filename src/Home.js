e80f60c4f275cc7f856767af2553081093d3b4ef
meinhoonharsh <harshprogrammer782@gmail.com>
2023-03-21 02:56:22 +0530

    Updated UI

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import jsondata from "./data.json";
+import image from "./jeetpakki.png";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
@@ -70,30 +71,35 @@ export default function Home() {
   return (
     <div>
       <div className="navbar">
-        <div className="logo">JSR</div>
+        <div className="logo">Gyan Wallah</div>
         <div className="nav-links">
-          <button class="button">Home</button>
+          <a class="button">Github</a>
         </div>
       </div>
 
       <div className="hero">
-        <div className="hero-text">
-          <h1>
-            Understand <span className="yellowspan">Any Topic</span> <br /> in
-            just <span className="pinkspan">One</span> Click
-          </h1>
+        <div className="hero-left">
+          <div className="hero-text">
+            <h1>
+              Understand <span className="yellowspan">Any Topic</span> <br /> in
+              just <span className="pinkspan">One</span> Click
+            </h1>
+          </div>
+          {/* Input with button */}
+          <div className="input-holder">
+            <input
+              type="text"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="Search for a topic"
+            />
+            <button className="search-button" onClick={handleLearnClick}>
+              Get Gyan!
+            </button>
+          </div>
         </div>
-        {/* Input with button */}
-        <div className="input-holder">
-          <input
-            type="text"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-            placeholder="Search for a topic"
-          />
-          <button className="search-button" onClick={handleLearnClick}>
-            Learn
-          </button>
+        <div className="hero-right">
+          <img src={image} />
         </div>
       </div>
 
