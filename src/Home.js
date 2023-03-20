7e866f86a47c7efffb20e2598ab396bce92e8b4e
meinhoonharsh <harshprogrammer782@gmail.com>
2023-03-20 23:02:21 +0530

    Added Slider

--- src/Home.js
+++ src/Home.js
@@ -94,7 +94,7 @@ export default function Home() {
       </div>
 
       {/* Slides */}
-      {/* {showSlides && <Slides data={data} />} */}
+      {showSlides && <Slides data={data} />}
     </div>
   );
 }
--- src/Slides.js
+++ src/Slides.js
@@ -1,27 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 export default function Slides({ data }) {
   var images = [];
 
   const getImageFromPrompt = async (prompt) => {
     axios
-      .post("https://stablediffusionapi.com/api/v3/text2img", {
-        key: "izBBAFO0G84bXNB2Vh7KR0kYPirfEZzYgaBAci5LYRqINDf5IVN1eohPBJSt",
-        prompt:
-          "A coil of wire with electric current and a magnet with rotating shaft",
-        negative_prompt:
-          "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime",
-        width: "512",
-        height: "512",
-        samples: "1",
-        num_inference_steps: "20",
-        seed: null,
-        guidance_scale: 7.5,
-        safety_checker: "yes",
-        webhook: null,
-        track_id: null,
-      })
+      .post("http://stablediffusionapi.com/api/v3/text2img")
       .then((res) => {
         console.log(res.data);
       })
@@ -50,24 +38,30 @@ export default function Slides({ data }) {
   });
   return (
     <div className="slides">
-      <div className="slide layout1">
-        <div className="slide-text">
-          Image of a computer chip with binary code on the screen to show how
-          computers use binary
+      <Carousel>
+        <div>
+          <div className="slide layout1">
+            <div className="slide-text">
+              Image of a computer chip with binary code on the screen to show
+              how computers use binary
+            </div>
+            <div className="slide-image">
+              <img src="https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/26e83d8b-6c01-4b5b-8cdc-8ab337939830-0.png" />
+            </div>
+          </div>
         </div>
-        <div className="slide-image">
-          <img src="https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/26e83d8b-6c01-4b5b-8cdc-8ab337939830-0.png" />
+        <div>
+          <div className="slide layout1">
+            <div className="slide-text">
+              Image of a computer chip with binary code on the screen to show
+              how computers use binary
+            </div>
+            <div className="slide-image">
+              <img src="https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/26e83d8b-6c01-4b5b-8cdc-8ab337939830-0.png" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="slide layout1">
-        <div className="slide-text">
-          Image of a computer chip with binary code on the screen to show how
-          computers use binary
-        </div>
-        <div className="slide-image">
-          <img src="https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/26e83d8b-6c01-4b5b-8cdc-8ab337939830-0.png" />
-        </div>
-      </div>
+      </Carousel>
     </div>
   );
 }
